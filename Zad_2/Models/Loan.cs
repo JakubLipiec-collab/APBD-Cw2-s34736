5aa93002d8cbcba2d9d37003556a1c563efb2324
Jakub Lipiec <s34736@pjwstk.edu.pl>
2026-03-24 20:46:47 +0100

    feat(domain): add Loan class

---
+++ Zad_2/Models/Loan.cs
@@ -0,0 +1,27 @@
+﻿namespace Zad_2.Models;
+
+public class Loan
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public User Borrower { get; private set; }
+    public Equipment Item { get; private set; }
+    public DateTime RentDate { get; private set; }
+    public DateTime DueTime  { get; private set; }
+    public DateTime ReturnDate { get; private set; }
+    public decimal PenaltyAmount { get; private set; }
+
+    public bool IsActive => ReturnDate == null;
+    public bool IsOverdue => IsActive && DateTime.Now > DueTime;
+    
+    public Loan(User borrower, Equipment item, int rentDays){
+        Borrower = borrower;
+        Item = item;
+        RentDate = DateTime.Now;
+        DueTime = RentDate.AddDays(rentDays);
+    }
+    
+    public void CompleteLoan(decimal penaltyAmount){
+        PenaltyAmount = penaltyAmount;
+        ReturnDate = DateTime.Now;
+    }
+}
