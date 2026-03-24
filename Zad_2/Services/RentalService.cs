62ca170f01637d123a6fcee7b73dc43272fa8ec4
Jakub Lipiec <s34736@pjwstk.edu.pl>
2026-03-24 21:35:12 +0100

    feat(rental): implement RentalService with IRentalService interface

---
+++ Zad_2/Services/RentalService.cs
@@ -0,0 +1,58 @@
+﻿using Zad_2.Models;
+using Zad_2.Exceptions;
+using Zad_2.Repositories;
+
+namespace Zad_2.Services
+{
+    public interface IRentalService
+    {
+        Loan RentEquipment(User user, Equipment item, int days);
+        decimal ReturnEquipment(Loan loan);
+    }
+    public class RentalService : IRentalService
+    {
+        private readonly MemoryRepository _repository;
+        private const decimal DailyPenalty = 10.0m;
+
+        public RentalService(MemoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Loan RentEquipment(User user, Equipment item, int days)
+        {
+            if (!item.isAvaiable)
+            {
+                throw new EquipmentUnavaiableException("Sprzet jest aktualnie niedostepny");
+            }
+            var activeLoans = _repository.GetActiveLoans(user);
+            if (activeLoans.Count >= user.MaxActiveLoans)
+            {
+                throw new LimitExceededException("Uzytkownik osiagnal limit wypozyczen");
+            }
+            var loan = new Loan(user, item, days);
+            item.SetAvaiable(false);
+            _repository.AddLoan(loan);
+            return loan;
+        }
+
+        public decimal ReturnEquipment(Loan loan)
+        {
+            if (!loan.IsActive)
+            {
+                throw new InvalidOperationException("To wypozyczenie zostalo juz zakonczone");
+            }
+
+            decimal penalty = 0;
+            if (DateTime.Now > loan.DueTime)
+            {
+                int dayslate = (DateTime.Now - loan.DueTime).Days;
+                penalty = dayslate*DailyPenalty;
+            }
+            loan.CompleteLoan(penalty);
+            loan.Item.SetAvaiable(true);
+            
+            return penalty;
+        }
+    }
+}
