1e7e75627cd8384c6f5e3d02956d1a7038f8820a
Jakub Lipiec <s34736@pjwstk.edu.pl>
2026-03-24 21:09:13 +0100

    feat(exceptions): add project specific exceptions

---
+++ Zad_2/Exceptions/EquipmentUnavaiableException.cs
@@ -0,0 +1,6 @@
+﻿namespace Zad_2.Exceptions;
+
+public class EquipmentUnavaiableException : Exception
+{
+    public EquipmentUnavaiableException(string message) : base(message) {}
+}
---
+++ Zad_2/Exceptions/LimitExceededException.cs
@@ -0,0 +1,6 @@
+﻿namespace Zad_2.Exceptions;
+
+public class LimitExceededException : Exception
+{
+    public LimitExceededException(string message) : base(message) {}
+}
