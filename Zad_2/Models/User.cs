688c8546dee9218a2af55edc44c6ae88d7289bd3
Jakub Lipiec <s34736@pjwstk.edu.pl>
2026-03-24 20:16:02 +0100

    feat(domain): add base User class

---
+++ Zad_2/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Zad_2.Models;
+
+public abstract class User
+{
+   public Guid Id { get; private set; } =  Guid.NewGuid();
+   public string Name { get; private set; }
+   public string LastName { get; private set; }
+    
+   public abstract int MaxActiveLoans { get; }
+
+   protected User(String name, string lastName)
+   {
+      if (String.IsNullOrEmpty(name) || String.IsNullOrEmpty(lastName))
+         throw new ArgumentException("Name and last name cannot be empty");
+      Name = name;
+      LastName = lastName;
+   }
+
+   public override string ToString()
+   {
+      return "Id: " + Id + ", Name: " + Name + ", LastName: " + LastName;
+   }
+}
