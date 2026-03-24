ab4d8a6376778c452aa06c87d3f94deb67d2ce30
Jakub Lipiec <s34736@pjwstk.edu.pl>
2026-03-24 20:34:54 +0100

    feat(domain): add base equipment class

---
+++ Zad_2/Models/Equipment.cs
@@ -0,0 +1,23 @@
+﻿namespace Zad_2.Models;
+
+public abstract class Equipment
+{
+    public Guid Id { get; private set; } =  Guid.NewGuid();
+    public string Name { get; private set; }
+    public bool isAvaiable { get; private set; } = true;
+    
+    protected Equipment(string name){
+        if(string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Equipment name can not be empty");
+        Name = name;
+    }
+
+    public void SetAvaiable(bool avaiable)
+    {
+        isAvaiable = avaiable;
+    }
+
+    public override string ToString()
+    {
+        return Id+" "+Name+" "+isAvaiable;
+    }
+}
